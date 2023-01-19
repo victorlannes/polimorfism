548313c2aa3076dfa0d359b6827d2d5a3dfb44e2
Victor Lannes <victor.ferreiralannes@hotmail.com>
2023-01-19 14:31:22 -0300

    Adicionar arquivos de projeto.

---
+++ polimorfism/Entities/OutSourcedEmployee.cs
@@ -0,0 +1,18 @@
+﻿namespace polimorfism.Entities {
+     class OutSourcedEmployee : Employee{
+
+        public double AdditionalCharge { get; set; }
+
+        public OutSourcedEmployee() 
+        {
+        }
+
+        public OutSourcedEmployee( string name, int hours, double valuePerHour,double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+        public override double Payment() {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+    }
+}
